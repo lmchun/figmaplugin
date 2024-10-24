61414cd218b3c7b5065b322d6bd6fa287ff109df
lmchun <lmchun@sbcglobal.net>
2024-10-23 21:32:10 -0700

    starting typescript section

--- social-media-post/code.ts
+++ social-media-post/code.ts
@@ -1,3 +1,15 @@
 figma.showUI(__html__);
 
 figma.ui.resize(500,500);
+
+figma.ui.onmessage = pluginMessage => {
+    console.log(pluginMessage.name)
+
+    figma.createRectangle();
+    if(pluginMessage.darkModeState === true){
+        console.log("dark")
+    }else{
+        console.log("light")
+    }
+    // figma.closePlugin();
+}
