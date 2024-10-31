da5841ed00f3b1b9af8ca32d50391609fe6cc7ae
lmchun <lmchun@sbcglobal.net>
2024-10-30 19:48:38 -0700

    finished plug in

--- social-media-post/code.js
+++ social-media-post/code.js
@@ -1,8 +1,18 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 figma.loadAllPagesAsync();
 figma.showUI(__html__);
 figma.ui.resize(500, 500);
-figma.ui.onmessage = pluginMessage => {
+figma.ui.onmessage = (pluginMessage) => __awaiter(void 0, void 0, void 0, function* () {
+    yield figma.loadFontAsync({ family: "Rubik", style: "Regular" });
     const postComponentSet = figma.root.findOne(node => node.type == "COMPONENT_SET" && node.name == "post");
     let selectedVariant;
     console.log(pluginMessage.imageVariant);
@@ -33,7 +43,14 @@ figma.ui.onmessage = pluginMessage => {
                 break;
         }
     }
-    selectedVariant.createInstance();
+    const newPost = selectedVariant.createInstance();
+    const templateName = newPost.findOne(node => node.name == "displayName" && node.type == "TEXT");
+    const templateUsername = newPost.findOne(node => node.name == "@username" && node.type == "TEXT");
+    const templateDescription = newPost.findOne(node => node.name == "description" && node.type == "TEXT");
+    templateName.characters = pluginMessage.name;
+    templateUsername.characters = pluginMessage.username;
+    templateDescription.characters = pluginMessage.description;
+    console.log(templateName + " aka " + templateUsername + " said" + templateDescription);
     // add back in when done developing
     // figma.closePlugin();
-};
+});
--- social-media-post/code.ts
+++ social-media-post/code.ts
@@ -3,13 +3,16 @@ figma.showUI(__html__);
 
 figma.ui.resize(500,500);
 
-figma.ui.onmessage = pluginMessage => {
+figma.ui.onmessage = async pluginMessage => {
+
+    await figma.loadFontAsync({ family: "Rubik", style: "Regular" }); 
 
     const postComponentSet = figma.root.findOne(node => node.type == "COMPONENT_SET" && node.name == "post") as ComponentSetNode;
     
     let selectedVariant;
 
     console.log(pluginMessage.imageVariant);
+
 
 
     // figma.createRectangle();
@@ -38,7 +41,17 @@ figma.ui.onmessage = pluginMessage => {
             break;   
         }
     }
-    selectedVariant.createInstance();
+
+    const newPost = selectedVariant.createInstance();
+    const templateName = newPost.findOne(node => node.name == "displayName" && node.type == "TEXT") as TextNode;
+    const templateUsername = newPost.findOne(node => node.name == "@username" && node.type == "TEXT") as TextNode;
+    const templateDescription = newPost.findOne(node => node.name == "description" && node.type == "TEXT") as TextNode;
+
+    templateName.characters = pluginMessage.name;
+    templateUsername.characters = pluginMessage.username;
+    templateDescription.characters = pluginMessage.description;
+
+    console.log(templateName + " aka " + templateUsername + " said" +templateDescription )
 
     // add back in when done developing
     // figma.closePlugin();
