30e93e23a730608761e2b881bdc1076afd241a96
lmchun <lmchun@sbcglobal.net>
2024-10-30 19:26:14 -0700

    fixed the issue somehow and starting to make different variants based on dark mode button

--- social-media-post/code.js
+++ social-media-post/code.js
@@ -1,14 +1,25 @@
 "use strict";
+figma.loadAllPagesAsync();
 figma.showUI(__html__);
 figma.ui.resize(500, 500);
 figma.ui.onmessage = pluginMessage => {
-    console.log(pluginMessage.name);
-    figma.createRectangle();
+    const postComponentSet = figma.root.findOne(node => node.type == "COMPONENT_SET" && node.name == "post");
+    const defaultVariant = postComponentSet.defaultVariant;
+    const defaultDark = postComponentSet.findOne(node => node.type == "COMPONENT" && node.name == "Image=none, Dark mode=true");
+    // this needs to exactly match the component on the file for both of these
+    // defaultVariant.createInstance();
+    // console.log(postComponentSet);
+    // console.log(postComponentSet.children);
+    // console.log(postComponentSet.name);
+    // console.log(pluginMessage.name)
+    // figma.createRectangle();
     if (pluginMessage.darkModeState === true) {
-        console.log("dark");
+        // console.log("dark")
+        defaultDark.createInstance();
     }
     else {
-        console.log("light");
+        // console.log("light")
+        defaultVariant.createInstance();
     }
     //at 26:46 on part 4
     // add back in when done developing
--- social-media-post/code.ts
+++ social-media-post/code.ts
@@ -1,15 +1,29 @@
+figma.loadAllPagesAsync()
 figma.showUI(__html__);
 
 figma.ui.resize(500,500);
 
 figma.ui.onmessage = pluginMessage => {
-    console.log(pluginMessage.name)
 
-    figma.createRectangle();
+    const postComponentSet = figma.root.findOne(node => node.type == "COMPONENT_SET" && node.name == "post") as ComponentSetNode;
+    const defaultVariant = postComponentSet.defaultVariant as ComponentNode;
+    const defaultDark = postComponentSet.findOne(node => node.type == "COMPONENT" && node.name == "Image=none, Dark mode=true") as ComponentNode;
+// this needs to exactly match the component on the file for both of these
+
+
+    // defaultVariant.createInstance();
+    // console.log(postComponentSet);
+    // console.log(postComponentSet.children);
+    // console.log(postComponentSet.name);
+    // console.log(pluginMessage.name)
+
+    // figma.createRectangle();
     if(pluginMessage.darkModeState === true){
-        console.log("dark")
+        // console.log("dark")
+        defaultDark.createInstance();
     }else{
-        console.log("light")
+        // console.log("light")
+        defaultVariant.createInstance();
     }
     //at 26:46 on part 4
     // add back in when done developing
